ed268a4dbe247b459055b0d227e077df9a3d544c
Marcelo <joao69@aluno.senai-ce.org.br>
2025-06-13 17:26:17 -0300

    feat: adicionar estrutura de grid para botões no componente Hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Grid, styled, Container, Typography, Button } from '@mui/material';
+import { Grid, styled, Container, Typography, Button, Box } from '@mui/material';
 import '@sakun/system.css';
 import Avatar from '../../../../img/Avatar.png';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
@@ -6,7 +6,7 @@ import EmailIcon from '@mui/icons-material/Email';
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        
+
         height: "100vh",
     }))
 
@@ -19,14 +19,25 @@ const Hero = () => {
             <StyledHero>
                 <Container >
                     <Grid container spacing={2}>
-                        <Grid size={4}>
+                        <Grid size={{ xs: 12, md: 4 }}>
                             <StyledImg src={Avatar} alt="Avatar" />
                         </Grid>
-                        <Grid size={8}>
+                        <Grid size={{ xs: 12, md: 8 }}>
                             <Typography variant="h1" color="primary" textAlign={"center"}>João Marcelo Carneiro</Typography>
                             <Typography variant="h2" color="primary" textAlign={"center"}>Desenvolvedor Front-End</Typography>
-                            <Button ><FileDownloadIcon />Download CV</Button>
-                            <Button ><EmailIcon />Contact</Button>
+
+                            <Grid container>
+                                <Grid>
+                                    <Box>
+                                        <Button ><FileDownloadIcon />Download CV</Button>
+                                    </Box>
+                                </Grid>
+                                <Grid>
+                                    <Box>
+                                        <Button ><EmailIcon />Contact</Button>
+                                    </Box>
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Container>
