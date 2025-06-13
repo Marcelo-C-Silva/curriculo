0f93681eb166be7385471b1d73968511b8eb0aa3
marcelo <marcelocarneirodev@gmail.com>
2025-06-13 10:01:13 -0300

    feat: estruturar componente Hero com imagem e informações de contato

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,20 +1,35 @@
-import { styled } from '@mui/material';
+import { Grid, styled, Container, Typography, Button } from '@mui/material';
 import '@sakun/system.css';
-
+import Avatar from '../../../../img/Avatar.png';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import EmailIcon from '@mui/icons-material/Email';
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "rgba(184, 185, 190, 0.5)" ,
+        height: "100vh",
     }))
 
     const StyledImg = styled("img")(() => ({
-        
+        width: "100%",
     }))
 
     return (
         <>
             <StyledHero>
-                ola
+                <Container >
+                    <Grid container spacing={2}>
+                        <Grid size={4}>
+                            <StyledImg src={Avatar} alt="Avatar" />
+                        </Grid>
+                        <Grid size={8}>
+                            <Typography variant="h1" color="primary" textAlign={"center"}>João Marcelo Carneiro</Typography>
+                            <Typography variant="h2" color="primary" textAlign={"center"}>Desenvolvedor Front-End</Typography>
+                            <Button><FileDownloadIcon />Download CV</Button>
+                            <Button><EmailIcon />Contact</Button>
+                        </Grid>
+                    </Grid>
+                </Container>
             </StyledHero>
         </>
     )
